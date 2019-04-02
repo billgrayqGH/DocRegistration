e514cf9114dc4c190b4974e45bd3796be49a8419
billgrayqGH <bgquitalig@yahoo.com>
2019-04-02 16:57:50 -0400

    Add project files.

---
+++ DocRegistration/Controllers/DoctorController.cs
@@ -0,0 +1,51 @@
+﻿using DocRegistration.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace DocRegistration.Controllers
+{
+    public class DoctorController : Controller
+    {
+        // GET: Get database Doctor models to initialize the form elements and validation in the view
+        public ActionResult CreateDoctor(int id = 0)
+        {
+            Doctor docModel = new Doctor();
+            return View(docModel);
+        }
+
+        //GET: Retrieve all values in datase
+        public ActionResult GetData()
+        {
+            using (DbModels db = new DbModels())
+            {
+                List<Doctor> doctorList = db.Doctors.ToList<Doctor>();
+                return View(doctorList);
+            }
+        }
+
+        //POST: Create a new doctor record
+        [HttpPost]
+        public ActionResult CreateDoctor(Doctor docModel)
+        {
+            // Initiate the "DbModels" as the database context and add a Doctor record "dbModel" to database
+            using (DbModels dbModel = new DbModels())
+            {
+                if (docModel.DoctorID == 0)
+                {
+                    dbModel.Doctors.Add(docModel);
+                    dbModel.SaveChanges();
+                }
+
+            }
+            ModelState.Clear();
+            ViewBag.SuccessMessage = "Registered Successfully";
+            return View("CreateDoctor", new Doctor());
+
+        }
+    }
+    
+}
---
+++ DocRegistration/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DocRegistration")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DocRegistration")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8b6c48a0-93db-411b-a997-f1653ea6d7c3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
